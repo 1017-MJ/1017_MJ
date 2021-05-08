d3759ff6897c539c22baee63cfd5b9f768e7fc50
1017-MJ <83795714+1017-MJ@users.noreply.github.com>
2021-05-08 14:46:26 +0900

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,7 @@
+won = 10000
+usd = won * 1130
+print (usd)
+exchange_rate = (int(input("현재 환율 입력: ")))
+won = 10000
+usd = won * exchange_rate
+print(usd)
---
+++ 5.py
@@ -0,0 +1,11 @@
+fahrenheit = 100
+celsius = fahrenheit - 32
+celsius = celsius * 5
+celsius = celsius / 9
+print(celsius)
+x = 0
+y = 0
+x = int(input("정수 x를 입력하시오: "))
+y = int(input("정수 y를 입력하시오: "))
+sum = x + y
+print(sum)
