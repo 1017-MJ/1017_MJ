8d226e40f9b8a8caf79775dfee6925beea0c564f
1017-MJ <83795714+1017-MJ@users.noreply.github.com>
2021-05-10 20:12:25 +0900

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,5 @@
+grade = int(input("성적을 입력하시오: "))
+if grade >= 60:
+    print("합격")
+else: 
+    print("불합격")
---
+++ 9.py
@@ -0,0 +1,6 @@
+price = int(input("상품의 가격을 입력하세요: "))
+if price > 2000:
+   shipping_cost = 0
+else:
+    shipping_cost = 3000
+print(shipping_cost)
