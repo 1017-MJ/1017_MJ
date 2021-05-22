908b78a06e282177a3b50dc3a83feb12971e70df
1017-MJ <83795714+1017-MJ@users.noreply.github.com>
2021-05-22 19:18:37 +0900

    Add files via upload

---
+++ 24.py
@@ -0,0 +1,83 @@
+print("철수 : 안녕 영희야 뭐해?")
+print("영희 : 안녕 철수야, 숨만쉬고 있어.")
+
+for i in range(10):
+    print("철수: 안녕 영희양 뭐해?")
+    print("영희: 안녕 철수야, 그냥 있어.")
+
+i = 0
+for i in range(5):                 
+    print(i)
+    print("철수: 안녕 영희양 뭐해?")
+    print("영희: 안녕 철수야, 그냥 있어.")
+    
+    if i == 1:
+      continue
+    
+    print("워니: 안녕 철수와 영희야!")
+
+i = 0
+for i in range(100):
+    print(i)
+    print("철수: 안녕 영희양 뭐해?")
+    print("영희: 안녕 철수야, 그냥 있어.")
+    i = i + 1
+        
+    if i > 2 :
+      break
+
+i = 0
+while i < 3:              
+    print(i)  
+    print("철수: 안녕 영희양 뭐해?")
+    print("영희: 안녕 철수야, 그냥 있어.")
+    i = i + 1
+
+i = 0
+while True:               
+    print(i)
+    print("철수: 안녕 영희양 뭐해?")
+    print("영희: 안녕 철수야, 그냥 있어.")
+    i = i + 1
+        
+    if i > 2 :
+      break
+    
+    
+
+
+i = 0
+while 1 < 3 :
+    print(i)
+    print("철수: 안녕 영희양 뭐해?")
+    print("영희: 안녕 철수야, 그냥 있어.")
+    i = i + 1
+
+
+
+while True:               
+    print(i)
+    print("철수: 안녕 영희양 뭐해?")
+    print("영희: 안녕 철수야, 그냥 있어.")
+    i = i + 1
+
+
+
+for i in range(100):
+    print(i)
+    print("철수: 안녕 영희양 뭐해?")
+    print("영희: 안녕 철수야, 그냥 있어.")
+    
+    if i > 2 :
+      break
+
+
+for i in range(3):
+    print(i)
+    print("철수: 안녕 영희양 뭐해?")
+    print("영희: 안녕 철수야, 그냥 있어.")
+   
+    if i == 1 :               
+      continue                
+  
+    print("정수: 어이 친구들.. 안녕")
