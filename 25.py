b0ba183871146add718c04030f5ee1896edf23e2
1017-MJ <83795714+1017-MJ@users.noreply.github.com>
2021-05-24 03:39:30 +0900

    Add files via upload

---
+++ 25.py
@@ -0,0 +1,40 @@
+x = list()
+y = []
+
+print(x)
+print(y)
+
+
+x = [1,2,3,4]
+y = ["Hello", "World"]
+z = ["Bye", 1,2,3]
+
+print(x)
+print(y)
+print(z)
+
+
+x = [1,2,3,4]
+y = ["Hello", "World"]
+z = ["Bye", 1,2,3]
+
+print(x+y, "\n")
+
+
+x = [1,2,3,4]
+   # 0,1,2,3
+print(x[0]) 
+print(x[3])
+
+
+x = [1,2,3,4]
+   # 0,1,2,3
+x[3] = 10
+
+print(x, "\n")
+
+
+x = [1,2,3,4]
+   # 0,1,2,3
+
+print(x[4])
